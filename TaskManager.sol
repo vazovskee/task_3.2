7c5d46d18b570f3e35dfe3801617bc0b925f9bfc
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-15 22:01:30 +0300

    task structure created

--- TaskManager.sol
+++ TaskManager.sol
@@ -4,6 +4,14 @@ pragma AbiHeader expire;
 
 contract TaskManager {
 
+    struct Task {
+        string title;
+        uint32 creationTime;
+        bool isDone; 
+    }
+
+    mapping(int8 => Task) tasks;
+
     constructor() public {
         require(tvm.pubkey() != 0, 101);
         require(msg.pubkey() == tvm.pubkey(), 102);
