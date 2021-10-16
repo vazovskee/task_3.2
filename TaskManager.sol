561594dfc5f28482c35609aa4be2686d9f84b9f0
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-16 22:52:08 +0300

    some comments were added

--- TaskManager.sol
+++ TaskManager.sol
@@ -4,14 +4,14 @@ pragma AbiHeader expire;
 
 contract TaskManager {
 
-    int8 public undoneTasksAmount;
-    int8 private lastTaskNumber;
+    int8 public undoneTasksAmount;         // переменная для хранения количества открытых задач
+    int8 private lastTaskNumber;           // переменная для хранения номера последней задачи
     int8 private constant INT8_MAX = 127;
 
     struct Task {
-        string title;
-        uint32 creationTime;
-        bool isDone; 
+        string title;         // название задачи
+        uint32 creationTime;  // время добавления задачи
+        bool isDone;          // статус задачи: выполнена или нет
     }
 
     mapping(int8 => Task) public tasks;
@@ -28,41 +28,46 @@ contract TaskManager {
         _;
     }
 
+    // с помощью этого модификатора проверяем наличие в сопоставлении задачи с заданным номером
     modifier checkTaskExistence(int8 taskNumber) {
         require(tasks.exists(taskNumber), 201, "task with this number dosn't exist");
         _;
     }
 
+    // функция для добавление новой задачи в сопоставление
     function addTask(string title) public checkOwnerAndAccept {
-        require(lastTaskNumber < INT8_MAX, 202, "maximum number of tasks has been reached");
+        require(lastTaskNumber < INT8_MAX, 202, "maximum number of tasks has been reached"); // проверяем, допустимо ли добавление новой задачи
 
         lastTaskNumber++;
         tasks[lastTaskNumber] = Task(title, now, false);
-        undoneTasksAmount++;
+        undoneTasksAmount++; // т.к. новая задача является открытой по умолчанию
     }
 
+    // функция для удаления задачи из сопоставления
     function removeTask(int8 taskNumber)
         public
         checkOwnerAndAccept
         checkTaskExistence(taskNumber)
     {
         if (!tasks[taskNumber].isDone) {
-            undoneTasksAmount--;
+            undoneTasksAmount--;  // если удалённая задача была открытой, то их общее число уменьшается
         }
         delete tasks[taskNumber];
     }
-
+    
+    // функция, которая помечает задачу как выполненную
     function setTaskAsDone(int8 taskNumber)
         public
         checkOwnerAndAccept
         checkTaskExistence(taskNumber)
     {
-        require(!tasks[taskNumber].isDone, 203, "this task has already been done");
+        require(!tasks[taskNumber].isDone, 203, "this task has already been done"); // проверяем, является ли задача открытой
         
         undoneTasksAmount--;
         tasks[taskNumber].isDone = true;
     }
 
+    // функция для получения описания задачи по ключу
     function getTaskByNum(int8 taskNumber)
         public view
         checkTaskExistence(taskNumber)
